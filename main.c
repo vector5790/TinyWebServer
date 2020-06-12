810602706bfb98dd6cb180f7f48d1dd8b861d9b9
vector5790 <1011669365@qq.com>
2020-06-12 23:23:59 +0800

    add timer

--- main.c
+++ main.c
@@ -14,9 +14,11 @@
 #include "./http/http_conn.h"
 #include "./threadpool/threadpool.h"
 #include "./lock/lock.h"
+#include "./timer/lst_timer.h"
 #define MAX_FD 65536           //最大文件描述符
 #define MAX_EVENT_NUMBER 10000 //最大事件数
 #define BUFFER_SIZE 1024
+#define TIMESLOT 5             //最小超时单位
 
 struct fds{
     int epollfd;
@@ -25,6 +27,42 @@ struct fds{
 
 extern int addfd(int epollfd,int fd,int oneshot);
 extern int removefd(int epollfd,int fd);
+extern int setnonblocking(int fd);
+static int pipefd[2];
+static sort_timer_lst timer_lst;
+static int epollfd=0;
+
+//信号处理函数
+void sig_handler(int sig){
+    int save_errno=errno;
+    int msg=sig;
+    send(pipefd[1],(char*)&msg,1,0);
+    errno=save_errno;
+}
+//设置信号函数
+void addsig(int sig,void(handler)(int),bool restart=true){
+    struct sigaction sa;
+    memset(&sa,'\0',sizeof(sa));
+    sa.sa_handler=handler;
+    if(restart){
+        sa.sa_flags|=SA_RESTART;
+    }
+    sigfillset(&sa.sa_mask);
+    assert(sigaction(sig,&sa,NULL)!=-1);
+}
+//定时处理任务
+void timer_handler(){
+    
+    timer_lst.tick();
+    alarm(TIMESLOT);
+}
+
+void cb_func(client_data *user_data){
+    epoll_ctl(epollfd,EPOLL_CTL_DEL,user_data->sockfd,0);
+    assert(user_data);
+    close(user_data->sockfd);
+    http_conn::m_user_count--;
+}
 void show_error(int connfd,const char* info){
     printf("%s",info);
     send(connfd,info,strlen(info),0);
@@ -77,7 +115,26 @@ int main(int argc, char *argv[]){
 
     addfd(epollfd,listenfd,false);
     http_conn::m_epollfd=epollfd;
-    while(true){
+
+    //创建管道（定时器）
+    ret=socketpair(PF_UNIX,SOCK_STREAM,0,pipefd);
+    assert(ret!=-1);
+    //管道写端非阻塞
+    setnonblocking(pipefd[1]);
+    //管道读端ET非阻塞
+    addfd(epollfd,pipefd[0],false);
+    //传递给主循环的信号量，只考虑这两种
+    addsig(SIGALRM,sig_handler,false);
+    addsig(SIGTERM,sig_handler,false);
+    
+    client_data *users_timer=new client_data[MAX_FD];
+
+    bool timeout=false;
+    bool stop_server=false;
+    //每隔TIMESLOT时间触发SIGALRM信号
+    alarm(TIMESLOT);
+
+    while(!stop_server){
         int number = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
         if((number<0)&&(errno!=EINTR)){
             printf("epoll failure\n");
@@ -89,6 +146,7 @@ int main(int argc, char *argv[]){
             if(sockfd==listenfd){
                 struct sockaddr_in client_address;
                 socklen_t client_addrlength=sizeof(client_address);
+
                 int connfd=accept(listenfd,(struct sockaddr*)&client_address,&client_addrlength);
                 if(connfd<0){
                     printf("errno is: %d\n",errno);
@@ -100,33 +158,100 @@ int main(int argc, char *argv[]){
                 }
                 /*初始化客户连接*/
                 users[connfd].init(connfd,client_address);
+
+                users_timer[connfd].address=client_address;
+                users_timer[connfd].sockfd=connfd;
+                util_timer *timer=new util_timer;
+                timer->user_data=&users_timer[connfd];
+                timer->cb_func=cb_func;
+                time_t cur=time(NULL);
+                timer->expire=cur+3*TIMESLOT;
+                users_timer[connfd].timer=timer;
+                timer_lst.add_timer(timer);
             }
             else if(events[i].events&(EPOLLRDHUP|EPOLLHUP|EPOLLERR)){
                 /*有异常，直接关闭客户连接*/
-                users[sockfd].close_conn();
+                //users[sockfd].close_conn();
+                util_timer *timer=users_timer[sockfd].timer;
+                timer->cb_func(&users_timer[sockfd]);
+                if(timer){
+                    timer_lst.del_timer(timer);
+                }
             }
             //处理客户连接上接收到的数据
             else if(events[i].events&EPOLLIN){
+                util_timer *timer=users_timer[sockfd].timer;
+
                 if(users[sockfd].read()){
                     pool->append(users+sockfd);
+                    if(timer){
+                        time_t cur=time(NULL);
+                        timer->expire=cur+3*TIMESLOT;
+                        timer_lst.adjust_timer(timer);
+                    }
                 }
                 else{
-                    users[sockfd].close_conn();
+                    //users[sockfd].close_conn();
+                    timer->cb_func(&users_timer[sockfd]);
+                    if(timer){
+                        timer_lst.del_timer(timer);
+                    }
                 }
             }
             else if(events[i].events&EPOLLOUT){
+                util_timer *timer=users_timer[sockfd].timer;
+
                 if(!users[sockfd].write()){
-                    users[sockfd].close_conn();
+                   // users[sockfd].close_conn();
+                    timer->cb_func(&users_timer[sockfd]);
+                    if(timer){
+                        timer_lst.del_timer(timer);
+                    }
+                }
+                else{
+                    if(timer){
+                        time_t cur=time(NULL);
+                        timer->expire=cur+3*TIMESLOT;
+                        timer_lst.adjust_timer(timer);
+                    }
+                }
+            }
+            else if((sockfd==pipefd[0])&&(events[i].events&EPOLLIN)){
+                int sig;
+                char signals[1024];
+                ret=recv(pipefd[0],signals,sizeof(signals),0);
+                if(ret==-1||ret==0){
+                    continue;
+                }
+                else{
+                    for(int i=0;i<ret;++i){
+                        switch(signals[i]){
+                            case SIGALRM:{
+                                timeout=true;
+                                break;
+                            }
+                            case SIGTERM:{
+                                stop_server=true;
+                            }
+                        }
+                    }
                 }
             }
             else{
                 printf("something else happened\n");
             }
         }
+        if(timeout){
+            timer_handler();
+            timeout=false;
+        }
     }
     close(epollfd);
     close(listenfd);
+    close(pipefd[1]);
+    close(pipefd[0]);
     delete [] users;
+    delete[] users_timer;
     delete pool;
     return 0;
 }
---
+++ timer/lst_timer.h
@@ -0,0 +1,151 @@
+#ifndef LST_TIMER
+#define LST_TIMER
+
+#include <time.h>
+class util_timer;
+//用户数据结构：客户端socket地址、socket文件描述符，定时器
+struct client_data{
+    sockaddr_in address;
+    int sockfd;
+    util_timer *timer;
+};
+class util_timer{
+public:
+    util_timer():prev(NULL),next(NULL){}
+
+public:
+    time_t expire;/*任务的超时时间（绝对时间）*/
+    void (*cb_func)(client_data*);/*回调函数*/
+    client_data* user_data;/*连接资源*/
+    util_timer* prev;/*指向前一个定时器*/
+    util_timer* next;/*指向后一个定时器*/
+};
+/*定时器链表，一个升序、双向链表，且带有头节点和尾节点*/
+class sort_timer_lst{
+public:
+    sort_timer_lst():head(NULL),tail(NULL){}
+    /*链表被销毁时，删除其中所有的定时器*/
+    ~sort_timer_lst(){
+        util_timer* tmp=head;
+        while(tmp){
+            head=tmp->next;
+            delete tmp;
+            tmp=head;
+        }
+    }
+    /*将目标定时器添加到链表中*/
+    void add_timer(util_timer* timer){
+        if(!timer){
+            return ;
+        }
+        if(!head){
+            head=tail=timer;
+            return ;
+        }
+        if(timer->expire<head->expire){
+            timer->next=head;
+            head->prev=timer;
+            head=timer;
+            return;
+        }
+        add_timer(timer,head);  
+    }
+    /*调整定时器在链表中的位置。只考虑被调整的定时器的超时时间延长的情况，即该定时器需要往链表尾部移动*/
+    void adjust_timer(util_timer* timer){
+        if(!timer){
+            return ;
+        }
+        util_timer* tmp=timer->next;
+        /*被调整的目标定时器处在链表尾部，超时值仍然小于下一个定时器的超时值，不用调整*/
+        if(!tmp||(timer->expire<tmp->expire)){
+            return;
+        }
+        if(timer==head){
+            head=head->next;
+            head->prev=NULL;
+            timer->next=NULL;
+            add_timer(timer,head);
+        }
+        else{
+            timer->prev->next=timer->next;
+            timer->next->prev=timer->prev;
+            add_timer(timer,timer->next);
+        }
+    }
+    void del_timer(util_timer* timer){
+        if(!timer){
+            return;
+        }
+        /*只有一个目标定时器，即目标定时器*/
+        if((timer==head)&&(timer==tail)){
+            delete timer;
+            head=NULL;
+            tail=NULL;
+            return;
+        }
+        /*至少有两个定时器，目标定时器为链表的头节点*/
+        if(timer==head){
+            head=head->next;
+            head->prev=NULL;
+            delete timer;
+            return;
+        }
+        /*至少有两个定时器，目标定时器为链表的尾节点*/
+        if(timer==tail){
+            tail=tail->prev;
+            tail->next=NULL;
+            delete timer;
+            return;
+        }
+        //被删除的定时器在链表内部
+        timer->prev->next=timer->next;
+        timer->next->prev=timer->prev;
+        delete timer;
+    }
+    void tick(){
+        if(!head){
+            return;
+        }
+        time_t cur=time(NULL);/*获得系统当前时间*/
+        util_timer* tmp=head;
+        while(tmp){
+            if(cur<tmp->expire){
+                break;
+            }
+            //当前定时器到期，则调用回调函数，执行定时事件
+            tmp->cb_func(tmp->user_data);
+            head=tmp->next;
+            if(head){
+                head->prev=NULL;
+            }
+            delete tmp;
+            tmp=head;
+        }
+    }
+private:
+    void add_timer(util_timer* timer,util_timer* lst_head){
+        util_timer* prev=lst_head;
+        util_timer* tmp=prev->next;
+        while(tmp){
+            if(timer->expire<tmp->expire){
+                prev->next=timer;
+                timer->next=tmp;
+                tmp->prev=timer;
+                timer->prev=prev;
+                break;
+            }
+            prev=tmp;
+            tmp=tmp->next;
+        }
+        if(!tmp){
+            prev->next=timer;
+            timer->prev=prev;
+            timer->next=NULL;
+            tail=timer;
+        }
+    }
+private:
+    util_timer* head;
+    util_timer* tail;
+};
+#endif
